a6228d59d79de034c68082b188d27d800c44c82a
chaimae elmaaloum <chaimae.elmaaloum57@gmail.com>
2023-05-30 16:00:22 -0700

    chaimae

--- functions2.c
+++ functions2.c
@@ -55,10 +55,10 @@ char extra_c = 0, padd = ' ';
 /************************* PRINT NON PRINTABLE *************************/
 /**
  * print_non_printable - Prints ascii codes in hexa of non printable chars
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
+ * @types: List of arguments
+ * @buffer: Buffer array to handle print...
+ * @flags:  Calculates active flags..
+ * @width: get width..
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
@@ -95,13 +95,13 @@ int print_non_printable(va_list types, char buffer[],
 /************************* PRINT REVERSE *************************/
 /**
  * print_reverse - Prints reverse string.
- * @types: Lista of arguments
+ * @types: List of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
- * @width: get width
+ * @width: get width.
  * @precision: Precision specification
- * @size: Size specifier
- * Return: Numbers of chars printed
+ * @size: Size specifier.
+ * Return: Numbers of chars to be printed
  */
 
 int print_reverse(va_list types, char buffer[],
@@ -138,13 +138,13 @@ int print_reverse(va_list types, char buffer[],
 /************************* PRINT A STRING IN ROT13 *************************/
 /**
  * print_rot13string - Print a string in rot13.
- * @types: Lista of arguments
+ * @types: List of arguments.
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Numbers of chars printed
+ * @flags:  Calculates active flags.
+ * @width: get width.
+ * @precision: Precision specification.
+ * @size: Size specifier.
+ * Return: Numbers of chars printed.
  */
 int print_rot13string(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
